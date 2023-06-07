65582603b0bdfb4c9614183f1df781f2c0435859
shepel04 <95418153+shepel04@users.noreply.github.com>
2023-06-07 11:26:09 +0300

    Populated combo boxes

--- MVVM/ViewModel/SearchViewModel.cs
+++ MVVM/ViewModel/SearchViewModel.cs
@@ -6,7 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Printing;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +27,12 @@ namespace Car4You.MVVM.ViewModel
         // Properties bound to the controls in your view
         public List<string> Brands { get; set; }
         public List<string> Models { get; set; }
+        public List<string> Years { get; set; }
+        public List<string> Bodies { get; set; }
+        public List<string> Fuels { get; set; }
+        public List<string> Gears { get; set; }
+        public List<string> Drives { get; set; }
+        public List<string> Colors { get; set; }
 
         // Add more properties for other search parameters
 
@@ -44,14 +53,27 @@ namespace Car4You.MVVM.ViewModel
                 {
                     selectedBrand = value;
                     OnPropertyChanged(nameof(SelectedBrand));
-                    PopulateModels(); // Call the PopulateModels method when selected brand changes
+                    PopulateModels();
+                    Models.Sort();
                 }
             }
         }
         public string SelectedModel { get; set; }
         public string FromYear { get; set; }
         public string ToYear { get; set; }
-
+        public string FromPrice { get; set; }
+        public string ToPrice { get; set; }
+        public string SelectedBody { get; set; }
+        public string FromMileage { get; set; }
+        public string ToMileage { get; set; }
+        public string FromVolume { get; set; }
+        public string ToVolume { get; set; }
+        public string SelectedFuel { get; set; }
+        public string FromConsumption { get; set; }
+        public string ToConsumption { get; set; }
+        public string SelectedGear { get; set; }
+        public string SelectedDrive { get; set; }
+        public string SelectedColor { get; set; }
 
 
         public SearchViewModel()
@@ -61,9 +83,23 @@ namespace Car4You.MVVM.ViewModel
             // Initialize properties and commands
             Brands = new List<string>();
             Models = new List<string>();
+            Years = new List<string>();
+            Bodies = new List<string> { "Седан", "Хетчбек", "Ліфтбек", "Купе", "Мінівен", "Позашляховик", "Кабріолет", "Універсал" };
+            Fuels = new List<string> { "Бензин", "Дизель", " Газ / Бензин", "Гібрид", "Електро" };
+            Gears = new List<string> { "Ручна", "Автомат", "Варіатор", "Типтронік", "Робот" };
+            Drives = new List<string> { "Передній", "Задній", "Повний" };
+            Colors = new List<string> { "Чорний", "Білий", "Сірий", "Синій", "Червоний", "Зелений", "Жовтий" }; 
             SearchResults = new List<CarDTO>();
 
+            PoputateYears();          
+            
             PopulateBrands();
+            if (Brands != null)
+            {
+                Brands.Sort();
+            }
+
+            Brands.Add(string.Empty);
 
             //PropertyChanged += (sender, e) =>
             //{
@@ -72,7 +108,7 @@ namespace Car4You.MVVM.ViewModel
             //        PopulateModels();
             //    }
             //};
-            
+
             // Initialize the SearchCommand with a RelayCommand or another implementation of ICommand
             SearchCommand = new RelayCommand(o =>
             {
@@ -80,15 +116,31 @@ namespace Car4You.MVVM.ViewModel
             });
         }
 
-        private void PopulateBrands()
+        private void PoputateYears()
+        {
+            int startYear = 1900;
+            int endYear = 2023;
+           
+            for (int year = endYear; year >= startYear; year--)
+            {
+                Years.Add(year.ToString());
+            }
+
+            Years.Add(string.Empty);
+
+            
+        }
+
+        private async Task PopulateBrands()
         {
             Brands.Clear();
-            var brands = dbContext.Cars.Select(c => c.Brand).Distinct().ToList();
+            var brands = await Task.Run(() => dbContext.Cars.Select(c => c.Brand).Distinct().ToList());
             foreach (var brand in brands)
             {
                 Brands.Add(brand);
             }
         }
+        
 
         private void PopulateModels()
         {
@@ -106,6 +158,8 @@ namespace Car4You.MVVM.ViewModel
                 {
                     Models.Add(model);
                 }
+
+                Models.Add(string.Empty);
             }
         }
 
@@ -125,12 +179,22 @@ namespace Car4You.MVVM.ViewModel
 
             if (!string.IsNullOrEmpty(FromYear))
             {
-                query = query.Where(c => Int32.Parse(c.Year) >= Int32.Parse(FromYear));
+                query = query.Where(c => string.Compare(c.Year, FromYear) >= 0);
             }
 
             if (!string.IsNullOrEmpty(ToYear))
             {
-                query = query.Where(c => Int32.Parse(c.Year) <= Int32.Parse(ToYear));
+                query = query.Where(c => string.Compare(c.Year, ToYear) <= 0);
+            }
+
+            if (!string.IsNullOrEmpty(FromPrice))
+            {
+                query = query.Where(c => string.Compare(c.Price, FromPrice) >= 0);
+            }
+
+            if (!string.IsNullOrEmpty(ToPrice))
+            {
+                query = query.Where(c => string.Compare(c.Price, ToPrice) <= 0);
             }
 
             // Retrieve the search results from the database
