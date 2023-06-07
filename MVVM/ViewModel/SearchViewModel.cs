609643ea2a1a5a2ec2ffe73e084e8e1ad6613850
shepel04 <95418153+shepel04@users.noreply.github.com>
2023-06-07 23:32:30 +0300

    Added search panel

--- MVVM/ViewModel/SearchViewModel.cs
+++ MVVM/ViewModel/SearchViewModel.cs
@@ -2,12 +2,16 @@
 using Car4You.MVVM.Model;
 using Car4You.MVVM.Model.Data;
 using Car4You.MVVM.Model.DTO;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net.Http.Headers;
 using System.Printing;
 using System.Runtime.CompilerServices;
@@ -24,20 +28,32 @@ namespace Car4You.MVVM.ViewModel
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        public ObservableCollection<CarDTO> searchResults;
+
         // Properties bound to the controls in your view
-        public List<string> Brands { get; set; }
-        public List<string> Models { get; set; }
-        public List<string> Years { get; set; }
-        public List<string> Bodies { get; set; }
-        public List<string> Fuels { get; set; }
-        public List<string> Gears { get; set; }
-        public List<string> Drives { get; set; }
-        public List<string> Colors { get; set; }
+        public ObservableCollection<string> Brands { get; set; }
+        public ObservableCollection<string> Models { get; set; }
+        public ObservableCollection<string> Years { get; set; }
+        public ObservableCollection<string> Bodies { get; set; }
+        public ObservableCollection<string> Fuels { get; set; }
+        public ObservableCollection<string> Gears { get; set; }
+        public ObservableCollection<string> Drives { get; set; }
+        public ObservableCollection<string> Colors { get; set; }
 
         // Add more properties for other search parameters
 
         // Collection bound to the data grid to display search results
-        public List<CarDTO> SearchResults { get; set; }
+
+
+        public ObservableCollection<CarDTO> SearchResults
+        {
+            get { return searchResults; }
+            set
+            {
+                searchResults = value;
+                OnPropertyChanged(nameof(SearchResults));
+            }
+        }
 
         // Command bound to the search button
         public System.Windows.Input.ICommand SearchCommand { get; set; }
@@ -54,7 +70,7 @@ namespace Car4You.MVVM.ViewModel
                     selectedBrand = value;
                     OnPropertyChanged(nameof(SelectedBrand));
                     PopulateModels();
-                    Models.Sort();
+                    SortObservableCollection(Models);
                 }
             }
         }
@@ -80,26 +96,29 @@ namespace Car4You.MVVM.ViewModel
         {
             dbContext = new ApplicationContext(); // Replace with your DbContext class
 
-            // Initialize properties and commands
-            Brands = new List<string>();
-            Models = new List<string>();
-            Years = new List<string>();
-            Bodies = new List<string> { "Седан", "Хетчбек", "Ліфтбек", "Купе", "Мінівен", "Позашляховик", "Кабріолет", "Універсал" };
-            Fuels = new List<string> { "Бензин", "Дизель", " Газ / Бензин", "Гібрид", "Електро" };
-            Gears = new List<string> { "Ручна", "Автомат", "Варіатор", "Типтронік", "Робот" };
-            Drives = new List<string> { "Передній", "Задній", "Повний" };
-            Colors = new List<string> { "Чорний", "Білий", "Сірий", "Синій", "Червоний", "Зелений", "Жовтий" }; 
-            SearchResults = new List<CarDTO>();
+            SearchResults = new ObservableCollection<CarDTO>();
 
-            PoputateYears();          
-            
+            // Initialize properties and commands
+            Brands = new ObservableCollection<string>();
+            Models = new ObservableCollection<string>();
+            Years = new ObservableCollection<string>();
+            Bodies = new ObservableCollection<string> { " ", "Седан", "Хетчбек", "Ліфтбек", "Купе", "Мінівен", "Позашляховик", "Кабріолет", "Універсал" };
+            Fuels = new ObservableCollection<string> { " ", "Бензин", "Дизель", "Газ / Бензин", "Гібрид", "Електро" };
+            Gears = new ObservableCollection<string> { " ", "Ручна", "Автомат", "Варіатор", "Типтронік", "Робот" };
+            Drives = new ObservableCollection<string> { " ", "Передній", "Задній", "Повний" };
+            Colors = new ObservableCollection<string> { " ", "Чорний", "Білий", "Сірий", "Синій", "Червоний", "Зелений", "Жовтий" };
+
+
+            PoputateYears();
+
+            Brands.Add(string.Empty);
             PopulateBrands();
             if (Brands != null)
             {
-                Brands.Sort();
+                SortObservableCollection(Brands);
             }
 
-            Brands.Add(string.Empty);
+
 
             //PropertyChanged += (sender, e) =>
             //{
@@ -116,36 +135,48 @@ namespace Car4You.MVVM.ViewModel
             });
         }
 
+        private static ObservableCollection<string> SortObservableCollection(ObservableCollection<string> collection)
+        {
+            List<string> sortedList = new List<string>(collection);
+            sortedList.Sort();
+
+            // Очистите коллекцию и добавьте отсортированные элементы обратно в ObservableCollection
+            collection.Clear();
+            foreach (var item in sortedList)
+            {
+                collection.Add(item);
+            }
+            return collection;
+        }
+
         private void PoputateYears()
         {
             int startYear = 1900;
             int endYear = 2023;
-           
+
+            Years.Add(string.Empty);
             for (int year = endYear; year >= startYear; year--)
             {
                 Years.Add(year.ToString());
             }
-
-            Years.Add(string.Empty);
-
-            
         }
 
-        private async Task PopulateBrands()
+        private void PopulateBrands()
         {
             Brands.Clear();
-            var brands = await Task.Run(() => dbContext.Cars.Select(c => c.Brand).Distinct().ToList());
+            var brands = dbContext.Cars.Select(c => c.Brand).Distinct().ToList();
             foreach (var brand in brands)
             {
                 Brands.Add(brand);
             }
         }
-        
+
 
         private void PopulateModels()
         {
             Models.Clear();
 
+            Models.Add(string.Empty);
             if (!string.IsNullOrEmpty(SelectedBrand))
             {
                 var models = dbContext.Cars
@@ -159,59 +190,193 @@ namespace Car4You.MVVM.ViewModel
                     Models.Add(model);
                 }
 
-                Models.Add(string.Empty);
             }
         }
 
         private void Search()
         {
-            var query = dbContext.Cars.AsQueryable();
 
-            if (!string.IsNullOrEmpty(SelectedBrand))
+            using (var dbContext = new ApplicationContext())
             {
-                query = query.Where(c => c.Brand == SelectedBrand);
+                var query = dbContext.Cars.AsQueryable();
+
+
+
+                if (!string.IsNullOrEmpty(SelectedBrand))
+                {
+                    query = query.Where(c => c.Brand == SelectedBrand);
+                }
+
+                if (!string.IsNullOrEmpty(SelectedModel))
+                {
+                    query = query.Where(c => c.Model == SelectedModel);
+                }
+
+                if (!string.IsNullOrEmpty(FromYear))
+                {
+                    query = query.Where(c => string.Compare(c.Year, FromYear) >= 0);
+                }
+
+                if (!string.IsNullOrEmpty(ToYear))
+                {
+                    query = query.Where(c => string.Compare(c.Year, ToYear) <= 0);
+                }
+
+                // Both prices entered
+                if (!string.IsNullOrEmpty(FromPrice) && !string.IsNullOrEmpty(ToPrice))
+                {
+                    List<CarDTO> filteredCars = new List<CarDTO>();
+                    foreach (var car in query)
+                    {
+                        if (IsValidPriceBoth(car.Price, Int32.Parse(FromPrice), Int32.Parse(ToPrice)))
+                        {
+                            filteredCars.Add(car);
+                        }
+                    }
+                    query = filteredCars.AsQueryable();
+                } // Entered only FromPrice
+                else if (!string.IsNullOrEmpty(FromPrice))
+                {
+                    List<CarDTO> filteredCars = new List<CarDTO>();
+                    foreach (var car in query)
+                    {
+                        if (IsValidPriceFrom(car.Price, Int32.Parse(FromPrice)))
+                        {
+                            filteredCars.Add(car);
+                        }
+                    }
+                    query = filteredCars.AsQueryable();
+                } // Entered only ToPrice
+                else if (!string.IsNullOrEmpty(ToPrice))
+                {
+
+                    List<CarDTO> filteredCars = new List<CarDTO>();
+                    foreach (var car in query)
+                    {
+                        if (IsValidPriceTo(car.Price, Int32.Parse(ToPrice)))
+                        {
+                            filteredCars.Add(car);
+                        }
+                    }
+                    query = filteredCars.AsQueryable();
+                }
+
+                if (!string.IsNullOrEmpty(SelectedBody))
+                {
+                    query = query.Where(c => c.Body == SelectedBody);
+                }
+
+                if (!string.IsNullOrEmpty(FromMileage))
+                {
+                    query = query.Where(c => c.Mileage >= Double.Parse(FromMileage));
+                }
+
+                if (!string.IsNullOrEmpty(ToMileage))
+                {
+                    query = query.Where(c => c.Mileage <= Double.Parse(ToMileage));
+                }
+
+                if (!string.IsNullOrEmpty(FromVolume))
+                {
+                    query = query.Where(c => c.Volume >= float.Parse(FromVolume));
+                }
+
+                if (!string.IsNullOrEmpty(ToVolume))
+                {
+                    query = query.Where(c => c.Volume <= float.Parse(ToVolume));
+                }
+
+                if (!string.IsNullOrEmpty(SelectedFuel))
+                {
+                    query = query.Where(c => string.Compare(c.Fuel.Replace(" ", string.Empty), SelectedFuel) == 0);
+                }
+
+                if (!string.IsNullOrEmpty(FromConsumption))
+                {
+                    query = query.Where(c => c.Consumption >= float.Parse(FromConsumption));
+                }
+
+                if (!string.IsNullOrEmpty(ToConsumption))
+                {
+                    query = query.Where(c => c.Consumption <= float.Parse(ToConsumption));
+                }
+
+                if (!string.IsNullOrEmpty(SelectedGear))
+                {
+                    query = query.Where(c => c.Gear == SelectedGear);
+                }
+
+                if (!string.IsNullOrEmpty(SelectedDrive))
+                {
+                    query = query.Where(c => c.Drive == SelectedDrive);
+                }
+
+                if (!string.IsNullOrEmpty(SelectedColor))
+                {
+                    query = query.Where(c => c.Color == SelectedColor);
+                }
+                var results = new List<CarDTO>();
+                // Retrieve the search results from the database
+                foreach (var item in query)
+                {
+                    results.Add(item);
+                }
+                //var results = query.ToList();
+
+                // Update the SearchResults collection with the retrieved results
+                SearchResults.Clear();
+                foreach (var car in results)
+                {
+                    SearchResults.Add(car);
+                }
             }
 
-            if (!string.IsNullOrEmpty(SelectedModel))
+        }
+
+        bool IsValidPriceBoth(string priceString, int priceFrom, int priceTo)
+        {
+            if (int.TryParse(priceString.Replace(" ", string.Empty).Replace("$", string.Empty), out int productPrice))
             {
-                query = query.Where(c => c.Model == SelectedModel);
+                return productPrice >= priceFrom && productPrice <= priceTo;
             }
-
-            if (!string.IsNullOrEmpty(FromYear))
+            else
             {
-                query = query.Where(c => string.Compare(c.Year, FromYear) >= 0);
-            }
-
-            if (!string.IsNullOrEmpty(ToYear))
-            {
-                query = query.Where(c => string.Compare(c.Year, ToYear) <= 0);
-            }
-
-            if (!string.IsNullOrEmpty(FromPrice))
-            {
-                query = query.Where(c => string.Compare(c.Price, FromPrice) >= 0);
-            }
-
-            if (!string.IsNullOrEmpty(ToPrice))
-            {
-                query = query.Where(c => string.Compare(c.Price, ToPrice) <= 0);
-            }
-
-            // Retrieve the search results from the database
-            var results = query.ToList();
-
-            // Update the SearchResults collection with the retrieved results
-            SearchResults.Clear();
-            foreach (var car in results)
-            {
-                SearchResults.Add(car);
+                return false;
             }
         }
 
+        bool IsValidPriceFrom(string priceString, int price)
+        {
+            if (int.TryParse(priceString.Replace(" ", string.Empty).Replace("$", string.Empty), out int productPrice))
+            {
+                return productPrice >= price;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+    
+        // Separate method to validate the price format and compare with the given price
+        bool IsValidPriceTo(string priceString, int price)
+        {
+            if (int.TryParse(priceString.Replace(" ", string.Empty).Replace("$", string.Empty), out int productPrice))
+            {
+                return productPrice <= price;
+            }
+            else
+            {
+                // Handle invalid price format for c.Price
+                // For example, exclude products with invalid prices from the query
+                return false;
+            }
+        }
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
     }
+
 }
